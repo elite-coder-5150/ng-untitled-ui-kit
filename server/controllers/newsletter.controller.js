5528ddf15f8fb57b3b7affe8badb2506c3b62f58
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-05-15 15:50:57 -0500

    defined the getNewsletterById function

--- server/controllers/newsletter.controller.js
+++ server/controllers/newsletter.controller.js
@@ -1,8 +1,46 @@
-const createNewsletter = (newsletterData) => {
-    axios.post('/api/newsletter', newsletterData)
-        .then(response => {
-            console.log('newsletter created: ', response.data);
-        }).catch(error => {
-            console.error('error creating newsletter: ', error);
-        })
-}
+const createNewsletter = (req, res) => {
+    const { title, content } = req.body;
+
+    db.query('insert into newsletter (title, content) values (:title, :content)', [title, conent], (error, results) => {
+        if (error) {
+            console.error('error creating newsletter');
+            res.status(500).json({ error: 'failed to create newsletter' });
+        } else {
+            res.status(201).json({ message: 'Newsletter created successfully', newsletterId: results.insertId });
+        }
+    })
+};
+
+const getAllNewsletters = (req, res) => {
+    const sql = `
+        select * from newsletters 
+    `;
+
+    db.query(sql, (error, results) => {
+        if (error) {
+            console.error('error fetching newsletter', error);
+            res.status(500).json({ error: 'Couldn\'t fetch newsletter' });
+        } else {
+            res.status(200).json(results);
+        }
+    })
+};
+
+const getNewsletterById = (req, res) => {
+    const newsletterId = req.params.id;
+
+    const sql = `
+        select * from newsletters where id=?
+    `;
+
+    db.query(sql, [newsletterId], (error, results) => {
+        if (error) {
+            console.error('error fetching newsletter', error);
+            res.status(500).json({ error: 'error fetching newsletter' });
+        } else if (results.length === 0) {
+            res.status(404).json({ erorr: 'Newsletter not found' });
+        } else {
+            results.status(200).json(results[0]);
+        }
+    });
+};
