becf477166fbed6714da898f7a38e2c53cd91243
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-05-15 16:15:43 -0500

    defined the getAllUsers function

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -15,3 +15,17 @@ export const createUser = (req, res) => {
     })
 };
 
+export const getAllUsers = (req, res) => {
+    const sql = `
+        select * from users
+    `;
+
+    db.query(sql, (error, results) => {
+        if (error) {
+            console.error('error fetching users', error);
+            res.status(500).json({error: 'failed to fetch users'});
+        } else {
+            res.status(200).json(results);
+        }
+    })
+};
